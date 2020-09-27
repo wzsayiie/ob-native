05c741424d05e66ff152dddf6abff3f1a1c0290a
wzsayiie <wzsayiie@qq.com>
2020-09-27 18:42:36 +0800

    update visual studio project.

--- mainsrc/craise/craise/craise.h
+++ mainsrc/craise/craise/craise.h
@@ -9,11 +9,15 @@
 //|     #include "xx.h"
 //| }
 
-#include "ccont.h"
 #include "clist.h"
 #include "cobj.h"
+#include "cont.h"
 #include "cstr.h"
+#include "dearray.h"
+#include "hashmap.h"
 #include "imap.h"
 #include "io.h"
 #include "macro.h"
+#include "pod.h"
+#include "rbtree.h"
 #include "smap.h"

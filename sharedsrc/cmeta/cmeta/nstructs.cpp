9b6295c5b958696abf910c5519481cf36b6526fa
wzsayiie <wzsayiie@qq.com>
2021-01-27 18:04:43 +0800

    added support for 'nclass'.

--- sharedsrc/cmeta/cmeta/nstructs.cpp
+++ sharedsrc/cmeta/cmeta/nstructs.cpp
@@ -4,6 +4,7 @@
 
 struct _NStructInfo {
     const char *name;
+    int super;
     int size;
 };
 
@@ -17,50 +18,65 @@ static _NStructInfo *sList = NULL;
 static int sEnd = 0;
 static int sConfine = 0;
 
-static void _NAddStruct(_NStructInfo *info) {
+static _NStructInfo *_NReallocStructInfo(_NStructInfo *list, int count) {
+    auto size = sizeof(_NStructInfo) * count;
+    return (_NStructInfo *)realloc(list, size);
+}
+
+static _NStructInfo *_NEnsureStruct(const char *name) {
     if (sEnd == sConfine) {
         sConfine += EACH_ALLOC_NUM;
+        sList = _NReallocStructInfo(sList, sConfine);
 
         if (sEnd == 0) {
             sEnd = LIST_BEGIN;
         }
+    }
 
-        auto newSize = sizeof(_NStructInfo) * sConfine;
-        auto newList = (_NStructInfo *)realloc(sList, newSize);
-        if (newList) {
-            sList = newList;
+    //insertion sort followed.
+    int begin = LIST_BEGIN;
+    int end   = sEnd;
+    int index = (begin + end) / 2;
+    while (begin < end) {
+        int result = strcmp(name, sList[index].name);
+        if (result == 0) {
+            //the item already exists.
+            return sList + index;
+        }
+
+        if (result > 0) {
+            begin = index + 1;
         } else {
-            return;
+            end = index;
         }
+
+        index = (begin + end) / 2;
     }
 
-    //insertion sort here.
-    int index = LIST_BEGIN;
-    while (index < sEnd) {
-        if (strcmp(sList[index].name, info->name) > 0) {
-            break;
-        }
-        index += 1;
-    }
-
+    //insert new item.
     if (index < sEnd) {
         void *dst = sList + index + 1;
         void *src = sList + index;
         int   num = sEnd  - index;
         memmove(dst, src, num * sizeof(_NStructInfo));
     }
-    sList[index] = *info;
+    _NStructInfo info = {0};
+    info.name = name;
+    sList[index] = info;
     sEnd += 1;
-}
 
-nclink int NStructsBegin() {return LIST_BEGIN;}
-nclink int NStructsEnd  () {return sEnd;}
-
-nclink int NFindStruct(const char *name) {
-    if (!name || *name == '\0') {
-        return 0;
+    //update reference data of all items.
+    for (int n = LIST_BEGIN; n < sEnd; ++n) {
+        _NStructInfo *item = sList + n;
+        if (item->super != 0 && item->super >= index) {
+            item->super += 1;
+        }
     }
 
+    return sList + index;
+}
+
+static int _NSearchStruct(const char *name) {
     //binary search here.
     int begin = LIST_BEGIN;
     int end   = sEnd;
@@ -80,6 +96,28 @@ nclink int NFindStruct(const char *name) {
     return 0;
 }
 
+static void _NAddStruct(const char *name, const char *super, int size) {
+    if (super) {
+        _NEnsureStruct(super);
+    }
+
+    _NStructInfo *info = _NEnsureStruct(name);
+    if (super) {
+        info->super = _NSearchStruct(super);
+    }
+    info->size = size;
+}
+
+nclink int NStructsBegin() {return LIST_BEGIN;}
+nclink int NStructsEnd  () {return sEnd;}
+
+nclink int NFindStruct(const char *name) {
+    if (name && *name) {
+        return _NSearchStruct(name);
+    }
+    return 0;
+}
+
 static bool _NStructIndexable(int index) {
     return LIST_BEGIN <= index && index < sEnd;
 }
@@ -91,6 +129,13 @@ nclink const char *NStructName(int index) {
     return NULL;
 }
 
+nclink int NStructSuper(int index) {
+    if (_NStructIndexable(index)) {
+        return sList[index].super;
+    }
+    return 0;
+}
+
 nclink int NStructSize(int index) {
     if (_NStructIndexable(index)) {
         return sList[index].size;
@@ -99,18 +144,16 @@ nclink int NStructSize(int index) {
 }
 
 struct _NStructAdder {
-    _NStructAdder(const char *name, int size) {
-        _NStructInfo info = {0};
-        info.name = name;
-        info.size = size;
-
-        _NAddStruct(&info);
+    _NStructAdder(const char *name, const char *super, int size) {
+        _NAddStruct(name, super, size);
     }
 };
 
-#undef  nstruct
-#define nstruct(n, ...) \
-/**/    __nstruct(n, __VA_ARGS__); \
-/**/    static _NStructAdder __adder_##n(#n, nisizeof(n))
+#define __add_struct(n, z, s) static _NStructAdder __adder_##n(#n, z, s)
+
+#undef nstruct
+#undef nclass
+#define nstruct(n,    ...) __nstruct(n,    __VA_ARGS__); __add_struct(n, NULL, nisizeof(n))
+#define nclass( n, s, ...) __nclass (n, s, __VA_ARGS__); __add_struct(n, #s  , 0)
 
 #include "NEXPORT.h"
--- sharedsrc/cmeta/cmeta/nstructs.h
+++ sharedsrc/cmeta/cmeta/nstructs.h
@@ -6,5 +6,6 @@ nfunc(int, NStructsBegin, (void));
 nfunc(int, NStructsEnd  , (void));
 nfunc(int, NFindStruct  , (const char *name));
 
-nfunc(const char *, NStructName, (int index));
-nfunc(int         , NStructSize, (int index));
+nfunc(const char *, NStructName , (int index));
+nfunc(int         , NStructSuper, (int index));
+nfunc(int         , NStructSize , (int index));

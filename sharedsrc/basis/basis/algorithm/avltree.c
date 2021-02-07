f31da38f71fe907212514a737422a1783bc38bb4
wzsayiie <wzsayiie@qq.com>
2021-02-07 16:41:53 +0800

    added interfaces of functions of 'avltree'.

--- sharedsrc/basis/basis/algorithm/avltree.c
+++ sharedsrc/basis/basis/algorithm/avltree.c
@@ -1,1 +1,48 @@
 #include "avltree.h"
+
+void atinit(avltree *tree, cmpr cmp) {
+    cpinit(&tree->pool, szof(atnode));
+    slinit(&tree->idat);
+
+    tree->cmp  = cmp ;
+    tree->root = NULL;
+    tree->icur = 0;
+}
+
+void atdeinit(avltree *tree) {
+    cpdeinit(&tree->pool);
+    sldeinit(&tree->idat);
+
+    tree->cmp  = NULL;
+    tree->root = NULL;
+    tree->icur = 0;
+}
+
+void atinsert(avltree *tree, word key, word value) {
+}
+
+word atremove(avltree *tree, word key) {
+    return iw(0);
+}
+
+bool atexist(avltree *tree, word key) {
+    return false;
+}
+
+int atcount(avltree *tree) {
+    return 0;
+}
+
+void atpbgn(avltree *tree) {
+}
+
+void atrbgn(avltree *tree) {
+}
+
+bool atnext(avltree *tree) {
+    return false;
+}
+
+word atread(avltree *tree) {
+    return iw(0);
+}
--- sharedsrc/basis/basis/algorithm/avltree.h
+++ sharedsrc/basis/basis/algorithm/avltree.h
@@ -1,10 +1,42 @@
 #pragma once
 
-#include "def.h"
+#include "cerpool.h"
+#include "scalist.h"
 
 #if __cplusplus
 extern "C" {
 #endif
+
+typedef
+struct atnode atnode;
+struct atnode {
+    word    key  ;
+    word    value;
+    atnode *left ;
+    atnode *right;
+};
+
+//the balance tree with avl algorithm.
+typedef struct avltree {
+    cmpr    cmp ;
+    cerpool pool;
+    atnode *root;
+    scalist idat;
+    int     icur;
+} avltree;
+
+void atinit  (avltree *tree, cmpr cmp);
+void atdeinit(avltree *tree);
+
+void atinsert(avltree *tree, word key, word value);
+word atremove(avltree *tree, word key);
+bool atexist (avltree *tree, word key);
+int  atcount (avltree *tree);
+
+void atpbgn(avltree *tree);
+void atrbgn(avltree *tree);
+bool atnext(avltree *tree);
+word atread(avltree *tree);
 
 #if __cplusplus
 }

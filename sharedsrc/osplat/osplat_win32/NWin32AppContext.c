b636be20d25c837da24f0e794b9d3881102d5c46
wzsayiie <wzsayiie@qq.com>
2021-03-06 11:50:03 +0800

    added 'nwin32appcontext.h'.

---
+++ sharedsrc/osplat/osplat_win32/NWin32AppContext.c
@@ -0,0 +1,40 @@
+﻿#include "NWin32AppContext.h"
+
+static const UINT NWM_ACTION = WM_USER + 0x401;
+
+static HWND sMainHWND = NULL;
+
+void _NWINSetMainHWND(HWND hwnd)
+{
+    sMainHWND = hwnd;
+}
+
+bool _NWINWindowProc(UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (msg == NWM_ACTION)
+    {
+        NWINActionFunc func = (NWINActionFunc)wParam;
+        void *arg = (void *)lParam;
+        func(arg);
+
+        return true;
+    }
+    return false;
+}
+
+HWND NWINGetMainHWND(void)
+{
+    return sMainHWND;
+}
+
+void NWINPostAction(HWND hwnd, NWINActionFunc func, void *arg)
+{
+    if (!hwnd || !func)
+    {
+        return;
+    }
+
+    WPARAM wParam = (WPARAM)func;
+    LPARAM lParam = (LPARAM)arg;
+    PostMessageW(hwnd, NWM_ACTION, wParam, lParam);
+}
---
+++ sharedsrc/osplat/osplat_win32/NWin32AppContext.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <Windows.h>
+#include <stdbool.h>
+
+//NOTE: ui host need call these functions.
+void _NWINSetMainHWND(HWND hwnd);
+bool _NWINWindowProc(UINT msg, WPARAM wParam, LPARAM lParam);
+
+//user functions:
+
+HWND NWINGetMainHWND(void);
+
+typedef void (*NWINActionFunc)(void *arg);
+void NWINPostAction(HWND hwnd, NWINActionFunc func, void *arg);
